abd3af2ca29f9019640beac558486a6a6313b205
Sebastian <sebastiandevelops@gmail.com>
2022-02-07 21:03:04 +0200

    first commit

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Video from '../components/Video'
+
+const home = () => {
+    return (
+        <div>
+           <Navbar />
+           <Video />
+        </div>
+    )
+}
+
+export default home
